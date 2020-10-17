15f3677b2c105491c4d010df535eeebdf2432b1c
e_lconci <lucas.conci@naranja.com>
2020-10-17 18:18:08 -0300

    new component and service to login in auth0

--- login-app/src/app/app.module.ts
+++ login-app/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import the module from the SDK
+import { AuthModule } from '@auth0/auth0-angular';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    AuthModule.forRoot({
+      domain: 'dev-lcopvwfo.us.auth0.com',
+      clientId: 'qLwBZm1GWQEu4Kzu0CGvYjuVOJVUgA2u',
+      responseType: 'token id_token',
+      redirectUri: 'http://localhost:4200/callback',
+      scope: 'openid'
+    }),
     BrowserModule,
     AppRoutingModule
   ],
